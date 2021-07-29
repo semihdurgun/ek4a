f58e70fdbf3b073f2a8a4b79a2d5a6a74a5dd8ed
semihdurgun <semihdurgun55@gmail.com>
2021-07-29 17:12:41 +0300

    ..

--- econalix/econalix/settings.py
+++ econalix/econalix/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = 'nht9n#0^$-&eva7%b*_sr0c%$)d@$t#6xkgq51tmr1x9x7+i81'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ek4a.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'yurtdisi',
 ]
 
 MIDDLEWARE = [
--- econalix/yurtdisi/views.py
+++ econalix/yurtdisi/views.py
@@ -1,10 +1,13 @@
+from econalix.settings import BASE_DIR
 from django.shortcuts import render
 import json
+from pathlib import Path
+
 # Create your views here.
 
 def index(request, *args, **kwargs):
 
-    with open('.\\yunanistan_json.json', 'r') as f:
+    with open(BASE_DIR / 'yunanistan_json.json', 'r') as f:
       data = json.loads(f.read())
 
     return render(request, 'yurtdisi.html',{'data':data}) 

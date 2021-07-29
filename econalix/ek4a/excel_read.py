fa64273373566e830d1fa0d36894ef4af7c1d792
semihdurgun <semihdurgun55@gmail.com>
2021-07-29 17:47:32 +0300

    ek4a added..

--- econalix/ek4a/excel_read.py
+++ econalix/ek4a/excel_read.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pathlib import Path
 import json, os
+from econalix.settings import BASE_DIR
 
 def excel_read_and_save(pageName,year,week):
     try:
-        sheet_name='.\\.\\excel_file\\'+str(year)+'-'+str(week)+'.xlsx'
+        sheet_name = str(BASE_DIR) + '\\excel_file\\'+str(year)+'-'+str(week)+'.xlsx'
         xls = pd.read_excel(sheet_name,sheet_name=pageName)
         df = pd.DataFrame(xls, columns=xls.columns)
         df = df.assign( **df.select_dtypes(['datetime']).astype(str).to_dict('list') ).to_json(orient="records")
